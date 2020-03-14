3176a805655925cbada2b35759df23475a6d6b87
Dominik Rembis <dominik.rembis@gmail.com>
2020-03-14 18:27:50 +0100

    * Added magic method __call in class 'validator'

--- validator.php
+++ validator.php
@@ -1,7 +1,12 @@
 <?php
 namespace Validator;
 use Validator\Provider;
+use Validator\Helpers\Classes as C;
 
 class Validator extends Provider
 {
+    public function __call(string $method = NULL, array $arguments = NULL)
+    {
+        return call_user_func_array([C::namespace($method), $method], $arguments);
+    }
 }
