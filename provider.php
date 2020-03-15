30b5c96dc10693fb2e0ad8739ec45f4b0e682c84
Dominik Rembis <dominik.rembis@gmail.com>
2020-03-15 19:32:29 +0100

    * Added new method 'validationError' in provider class. Responsible for saved errors in array

--- provider.php
+++ provider.php
@@ -1,13 +1,21 @@
 <?php
 namespace Validator;
+use Validator\Helpers\Iterator as I;
 
 class Provider
 {
   protected static $validationData = NULL;
+  protected static $validationResult = [];
 
   public function make($validationData = NULL)
   {
     self::$validationData = $validationData;
+    I::readKey(self::$validationResult);
     return $this;
+  }
+
+  public function validationError($methodName = NULL)
+  {
+    self::$validationResult[I::key()][] = $methodName;
   }
 }
