3469b8758553c4a6571443ddbc62d8592881b7cf
Dominik Rembis <dominik.rembis@gmail.com>
2020-03-17 11:57:50 +0100

    * Added new method 'class' in class 'classes' - method responsible for returning class name in which it is located

--- helpers/classes.php
+++ helpers/classes.php
@@ -7,11 +7,20 @@ class Classes
         'Types' => ['array', 'bool', 'double', 'float', 'int', 'long', 'object', 'string']
     ];
 
-    public static function namespace(string $method = NULL)
+    public static function namespace(string $method = NULL) : string
     {
         foreach (self::$map as $class => $methodArray) {
             if (in_array($method, $methodArray)) {
                 return 'Validator\Modules\\'.$class; 
+            }
+        }
+    }
+
+    public static function class(string $method = NULL) : string
+    {
+        foreach (self::$map as $class => $methodArray) {
+            if (in_array($method, $methodArray)) {
+                return $class; 
             }
         }
     }
